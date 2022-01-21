e3488d30151be379433737a74a6dc26a8b9672cb
Jelle Spijker <spijker.jelle@gmail.com>
2022-01-21 07:51:27 +0100

    Initial importlib-metadata recipe
    Contributes to 8801

---
+++ recipes/importlib-metadata/conanfile.py
@@ -0,0 +1,54 @@
+import os
+
+from conans import tools
+from conan import ConanFile
+from conan.tools.env.virtualrunenv import VirtualRunEnv
+from conan.tools.env.virtualbuildenv import VirtualBuildEnv
+from conan.tools.files.packager import AutoPackager
+
+required_conan_version = ">=1.44.1"
+
+
+class ImportlibMetadataConan(ConanFile):
+    name = "importlib-metadata"
+    version = "4.10.1"
+    description = "Read metadata from Python packages"
+    topics = ("conan", "python", "pypi", "pip")
+    license = ""
+    homepage = "https://github.com/python/importlib_metadata"
+    url = "https://github.com/python/importlib_metadata"
+    settings = "os", "compiler", "build_type", "arch"
+    build_policy = "missing"
+    default_user = "python"
+    default_channel = "stable"
+    python_requires = "PipBuildTool/0.1@ultimaker/testing"
+    requires = "python/3.10.2@python/stable",\
+               "zipp/3.7.0@python/stable"
+    hashes = []
+
+    def layout(self):
+        self.folders.build = "build"
+        self.folders.package = "package"
+        self.folders.generators = os.path.join("build", "conan")
+
+    def generate(self):
+        rv = VirtualRunEnv(self)
+        rv.generate()
+
+        bv = VirtualBuildEnv(self)
+        bv.generate()
+
+    def build(self):
+        pb = self.python_requires["PipBuildTool"].module.PipBuildTool(self)
+        pb.configure()
+        pb.build()
+
+    def package(self):
+        packager = AutoPackager(self)
+        packager.patterns.lib = ["*.so", "*.so.*", "*.a", "*.lib", "*.dylib", "*.py*"]
+        packager.run()
+
+    def package_info(self):
+        v = tools.Version(self.dependencies['python'].ref.version)
+        self.runenv_info.prepend_path("PYTHONPATH", os.path.join(self.package_folder, "lib", f"python{v.major}.{v.minor}", "site-packages"))
+        self.buildenv_info.prepend_path("PYTHONPATH", os.path.join(self.package_folder, "lib", f"python{v.major}.{v.minor}", "site-packages"))
