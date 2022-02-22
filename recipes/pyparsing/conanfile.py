ad57d48d7c25816cf4be13bb6ad810e232e97564
j.spijker@ultimaker.com <jelle spijker>
2022-02-22 13:40:31 +0100

    Added recipe for pyparsing
    needed by packaging

---
+++ recipes/pyparsing/conanfile.py
@@ -0,0 +1,56 @@
+import os
+
+from conans import tools
+from conan import ConanFile
+from conan.tools.env.virtualrunenv import VirtualRunEnv
+from conan.tools.env.virtualbuildenv import VirtualBuildEnv
+from conan.tools.files.packager import AutoPackager
+
+required_conan_version = ">=1.44.1"
+
+
+class PyparsingConan(ConanFile):
+    name = "pyparsing"
+    version = "3.0.7"
+    description = "Python parsing module"
+    topics = ("conan", "python", "pypi", "pip")
+    license = "MIT License"
+    homepage = "https://github.com/pyparsing/pyparsing/"
+    url = "https://github.com/pyparsing/pyparsing/"
+    settings = "os", "compiler", "build_type", "arch"
+    build_policy = "missing"
+    default_user = "python"
+    default_channel = "stable"
+    python_requires = "PipBuildTool/0.2@ultimaker/testing"
+    requires = "python/3.10.2@python/stable"
+    hashes = []
+
+    def layout(self):
+        self.folders.build = "build"
+        self.folders.package = "package"
+        self.folders.generators = os.path.join("build", "conan")
+
+    def generate(self):
+        rv = VirtualRunEnv(self)
+        rv.generate()
+
+        bv = VirtualBuildEnv(self)
+        bv.generate()
+
+    def build(self):
+        pb = self.python_requires["PipBuildTool"].module.PipBuildTool(self)
+        pb.configure()
+        pb.build()
+
+    def package(self):
+        packager = AutoPackager(self)
+        packager.patterns.lib = ["*.so", "*.so.*", "*.a", "*.lib", "*.dylib", "*.py*"]
+        packager.run()
+
+    def package_info(self):
+        v = tools.Version(self.dependencies['python'].ref.version)
+        self.runenv_info.prepend_path("PYTHONPATH", os.path.join(self.package_folder, "lib", f"python{v.major}.{v.minor}", "site-packages"))
+        self.buildenv_info.prepend_path("PYTHONPATH", os.path.join(self.package_folder, "lib", f"python{v.major}.{v.minor}", "site-packages"))
+
+    def package_id(self):
+        self.info.settings.build_type = "Release"
