838ec48345fb5e54dd20d51153ba7eb5856629b3
j.spijker@ultimaker.com <jelle spijker>
2022-02-27 17:23:03 +0100

    Added ultimaker_layout to Ultimaker base file
    This is the layout most commonly used by ultimaker projects

--- recipes/ultimakerconanfile/conanfile.py
+++ recipes/ultimakerconanfile/conanfile.py
@@ -1,3 +1,5 @@
+import os
+
 from conans import ConanFile, tools
 from conans.errors import ConanException
 
@@ -32,9 +34,33 @@ class UltimakerBase(object):
                 else:
                     self.version += f"-a+{branch}.{rev}"  # only use rev
 
+    def ultimaker_layout(self, include_dir = "src"):
+        compiler = self.settings.get_safe("compiler")
+        multi = compiler in ("Visual Studio", "msvc")
+
+        self.folders.source = "."
+        try:
+            build_type = str(self.settings.build_type)
+        except ConanException:
+            raise ConanException("'build_type' setting not defined, it is necessary for cmake_layout()")
+        if multi:
+            self.folders.build = "build"
+        else:
+            comp = "" if compiler in ("gcc", "apple-clang") else f"-{compiler}"
+            build_type = build_type.lower()
+            self.folders.build = f"cmake-build-{build_type}{comp}"
+        self.folders.generators = os.path.join(self.folders.build, "conan")
+        self.cpp.source.includedirs = [include_dir]
+        if multi:
+            self.cpp.build.libdirs = [f"{build_type}"]
+            self.cpp.build.bindirs = [f"{build_type}"]
+        else:
+            self.cpp.build.libdirs = ["."]
+            self.cpp.build.bindirs = ["."]
+
 
 class Pkg(ConanFile):
     name = "UltimakerBase"
-    version = "0.1"
+    version = "0.2"
     default_user = "ultimaker"
     default_channel = "testing"
