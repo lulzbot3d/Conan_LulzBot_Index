e74c80b6ac7174a44c93612bffb613ef018eae87
j.spijker@ultimaker.com <jelle spijker>
2022-02-27 18:43:39 +0100

    Use the CMakeBuilder for sip
    This keeps the difference between normal and conan as small as possible
    
    Contributes to CURA-7924

--- recipes/sip/conanfile.py
+++ recipes/sip/conanfile.py
@@ -1,11 +1,8 @@
 import os
-import stat
-from pathlib import Path
 
 from jinja2 import Template
 
 from conans import tools
-from conans.model import Generator
 
 from conan import ConanFile
 from conan.tools.env.virtualrunenv import VirtualRunEnv
@@ -13,78 +10,6 @@ from conan.tools.env.virtualbuildenv import VirtualBuildEnv
 from conan.tools.files.packager import AutoPackager
 
 required_conan_version = ">=1.44.1"
-
-class sip(Generator):
-
-    @property
-    def filename(self):
-        pass
-
-    @property
-    def content(self):
-        ext_deps = []
-        deps_libs = []
-        deps_lib_dirs = []
-        deps_inc_dirs = []
-        deps_compile_args = []
-        deps_link_args = []
-        deps_defines = []
-
-        # Loop through all dependencies and collect needed necessary compile and link information
-        for dep_name, dep_cpp_info in self.deps_build_info.dependencies:
-            ext_deps.append(f'"{dep_name}"')
-
-            for dep in dep_cpp_info.libs:  # The libs to link against
-                deps_libs.append(f'"{dep}"')
-            for dep in dep_cpp_info.system_libs:  # System libs to link against
-                deps_libs.append(f'"{dep}"')
-
-            for dep in dep_cpp_info.lib_paths:  # Directories where libraries can be found
-                deps_lib_dirs.append(f'"{dep}"'.replace("\\", "/"))
-
-            for dep in dep_cpp_info.include_paths:  # Directories where headers can be found
-                deps_inc_dirs.append(f'"{dep}"'.replace("\\", "/"))
-
-            for dep in dep_cpp_info.cppflags:  # C++ compilation flags
-                deps_compile_args.append(f'"{dep}"')
-            for dep in dep_cpp_info.cflags:  # pure C flags
-                deps_compile_args.append(f'"{dep}"')
-
-            for dep in dep_cpp_info.sharedlinkflags:  # linker flags
-                deps_link_args.append(f'"{dep}"')
-            for dep in dep_cpp_info.exelinkflags:  # linker flags
-                deps_link_args.append(f'"{dep}"')
-
-            for dep in dep_cpp_info.defines:  # preprocessor definitions
-                deps_defines.append(f'"{dep}"')
-
-        # Python version binary compatible (major.minor)
-        python_version = tools.Version(self.conanfile.dependencies['python'].ref.version)
-
-        with open(os.path.join(Path(__file__).parent, "ConanBuilder.py"), "r") as f:
-            conan_builder_content = f.read()
-
-        with open(os.path.join(Path(__file__).parent, "pyproject.toml.pre.jinja"), "r") as f:
-            tm = Template(f.read())
-            pyproject_toml_content = tm.render(
-                module_version = self.conanfile.version,
-                module_homepage = self.conanfile.url if self.conanfile.url else "",
-                module_author = self.conanfile.author if self.conanfile.author else "",
-                module_license = self.conanfile.license if self.conanfile.license else "",
-                python_version = f"{python_version.major}.{python_version.minor}",
-                external_deps = ",".join(ext_deps) if len(ext_deps) > 0 else "",
-                deps_include_dirs = ",".join(deps_inc_dirs) if len(deps_inc_dirs) > 0 else "",
-                deps_libraries = ",".join(deps_libs) if len(deps_libs) > 0 else "",
-                deps_library_dirs = ",".join(deps_lib_dirs) if len(deps_lib_dirs) > 0 else "",
-                deps_compile_args = ",".join(deps_compile_args) if len(deps_lib_dirs) > 0 else "",
-                deps_link_args = ",".join(deps_link_args) if len(deps_link_args) > 0 else "",
-                deps_define_macros = ",".join(deps_defines) if len(deps_lib_dirs) > 0 else "",
-                build_type = "false" if self.settings.build_type == "Release" else "true")
-            return {
-                "pyproject.toml.pre": pyproject_toml_content,
-                "ConanBuilder.py": conan_builder_content
-            }
-
 
 class Pyqt6SipConan(ConanFile):
     name = "sip"
@@ -100,8 +25,8 @@ class Pyqt6SipConan(ConanFile):
     default_channel = "stable"
     python_requires = "PipBuildTool/0.2@ultimaker/testing"
     requires = "python/3.10.2@python/stable", "toml/0.10.2@python/stable", "packaging/21.3@python/stable"
-    exports = ["sip.cmake.jinja", "pyproject.toml.pre.jinja", "sip_buildscript.jinja", "ConanBuilder.py"]
-    exports_sources = ["sip.cmake.jinja", "pyproject.toml.pre.jinja", "sip_buildscript.jinja", "ConanBuilder.py"]
+    exports = ["sip.cmake.jinja", "CMakeBuilder.py"]
+    exports_sources = ["sip.cmake.jinja", "CMakeBuilder.py"]
     hashes = []
 
     def layout(self):
@@ -125,29 +50,13 @@ class Pyqt6SipConan(ConanFile):
         # Add the sip CMake build module
         with open(os.path.join(self.source_folder, "sip.cmake.jinja"), "r") as f:
             tm = Template(f.read())
-            result = tm.render(sip_build_script = self._sip_buildscript_path)
+            path_sep = ";" if self.settings.os == "Windows" else ":"
+            python_path = self._pythonpath
+            for dep in self.deps_user_info.values():
+                if hasattr(dep, "pythonpath"):
+                    python_path += path_sep + dep.pythonpath
+            result = tm.render(sip_build_script = self._sip_buildscript_path, conan_python_path = python_path)
             tools.save(os.path.join(self.package_folder, self._cmake_install_base_path, "sip.cmake"), result)
-
-        # Add the sip buildscript
-        with open(os.path.join(self.source_folder, "sip_buildscript.jinja"), "r") as f:
-            tm = Template(f.read())
-            sip_executable = str(os.path.join(self.package_folder, "bin", "sip-build"))
-
-            sip_build_env = ""
-            pre_script = ""
-            if self.settings.os == "Windows":
-                sip_executable += ".exe"
-                sip_build_env = f"env: PYTHONPATH+=;{self._pythonpath}"  # TODO: check on Windows
-            else:
-                pre_script = "#!/bin/bash"
-                sip_build_env = f"export PYTHONPATH={self._pythonpath}:$PYTHONPATH"
-
-            result = tm.render(pre_script = pre_script, sip_build_executable = sip_executable, sip_build_env = sip_build_env)
-            tools.save(self._sip_buildscript_path, result)
-
-        # set the executable bit TODO: check on Windows
-        st = os.stat(self._sip_buildscript_path)
-        os.chmod(self._sip_buildscript_path, st.st_mode | stat.S_IEXEC)
 
         # Add the rest of the sip files
         packager = AutoPackager(self)
@@ -157,6 +66,8 @@ class Pyqt6SipConan(ConanFile):
     def package_info(self):
         self.runenv_info.prepend_path("PYTHONPATH", self._pythonpath)
         self.buildenv_info.prepend_path("PYTHONPATH", self._pythonpath)
+        self.user_info.python_path = self._pythonpath
+
         self.cpp_info.set_property("cmake_file_name", "sip")
         self.cpp_info.set_property("cmake_target_name", "sip::sip")
         self.cpp_info.set_property("cmake_build_modules", [os.path.join(self._cmake_install_base_path, "sip.cmake")])
@@ -177,9 +88,7 @@ class Pyqt6SipConan(ConanFile):
 
     @property
     def _sip_buildscript_path(self):
-        sipbuild_script = "sip_buildscript"
+        sipbuild_script = "sip-build"
         if self.settings.os == "Windows":
-            sipbuild_script += ".ps1"
-        else:
-            sipbuild_script += ".sh"
+            sipbuild_script += ".exe"
         return os.path.join(self.package_folder, self._cmake_install_base_path, sipbuild_script)
