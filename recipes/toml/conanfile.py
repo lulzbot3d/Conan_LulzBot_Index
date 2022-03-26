7045324d876fd9d5095622e867c97b5b2fe9fe5e
Jelle Spijker <spijker.jelle@gmail.com>
2022-03-26 19:12:24 +0100

    Simplified toml recipe
    No need to use a helper tool as builder.
    de conan.tools.files module will automatically download and extract the wheel file
    and check the hash.
    Just provide the url and hash in the conandata.yml file

--- recipes/toml/conanfile.py
+++ recipes/toml/conanfile.py
@@ -1,13 +1,12 @@
+from pathlib import Path
 from conan import ConanFile
-from conan.tools.env.virtualrunenv import VirtualRunEnv
-from conan.tools.env.virtualbuildenv import VirtualBuildEnv
+from conan.tools.files import files
 
-required_conan_version = ">=1.44.1"
+required_conan_version = ">=1.33.0"
 
 
 class TomlConan(ConanFile):
     name = "toml"
-    version = "0.10.2"
     description = "Python Library for Tom's Obvious, Minimal Language"
     topics = ("conan", "python", "pypi", "pip")
     license = "MIT"
@@ -15,30 +14,22 @@ class TomlConan(ConanFile):
     url = "https://github.com/uiri/toml"
     settings = "os", "compiler", "build_type", "arch"
     build_policy = "missing"
-    default_user = "pypi"
-    default_channel = "stable"
-    python_requires = ["UltimakerBase/0.4@ultimaker/testing", "PipBuildTool/0.2@ultimaker/testing"]
-    python_requires_extend = "UltimakerBase.UltimakerBase"
-    requires = "python/3.10.2@python/stable"
-    hashes = []
+    requires = "cpython/[>=2.6.0]@python/stable"
+    no_copy_source = True
 
-    def generate(self):
-        rv = VirtualRunEnv(self)
-        rv.generate()
+    @property
+    def _site_packages(self):
+        return "site-packages"
 
-        bv = VirtualBuildEnv(self)
-        bv.generate()
-
-    def build(self):
-        pb = self.python_requires["PipBuildTool"].module.PipBuildTool(self)
-        pb.configure()
-        pb.build()
+    def source(self):
+        files.get(self, **self.conan_data["sources"][self.version], destination = self._site_packages)
 
     def package(self):
-        self.copy("*")
+        self.copy("*", src = self._site_packages, dst = self._site_packages)
 
     def package_info(self):
-        self._set_python_site_packages()
+        self.env_info.PYTHONPATH = Path(self.package_folder, self._site_packages)
+        self.user_info.pythonpath = Path(self.package_folder, self._site_packages)
 
     def package_id(self):
-        self.info.settings.build_type = "Release"
+        self.info.header_only()
