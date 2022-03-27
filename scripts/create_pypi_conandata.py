4f1310a40f5cc1265ef359eff7cc9fc2a8e1bc2e
j.spijker@ultimaker.com <jelle spijker>
2022-03-27 13:13:39 +0200

    Make sure that strings are quoted in the conandata.yml

--- scripts/create_pypi_conandata.py
+++ scripts/create_pypi_conandata.py
@@ -10,6 +10,15 @@ from pathlib import Path
 from docopt import docopt
 
 
+def quoted_presenter(dumper, data):
+    # define a custom representer for strings, needed because the Conan parse could otherwise interpret version numbers
+    # as numeric values. See https://docs.conan.io/en/1.45/reference/config_files/conandata.yml.html
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='"')
+
+
+yaml.add_representer(str, quoted_presenter)
+
+
 def main(name: str, location: str):
     resp = requests.get(f"https://pypi.org/pypi/{name}/json").json()
 
@@ -18,7 +27,7 @@ def main(name: str, location: str):
         for release, release_data in resp["releases"].items():
             for data in release_data:
                 if data["packagetype"] == "bdist_wheel":
-                    conandata[release] = {"url": data["url"], "sha256": data["digests"]["sha256"]}
+                    conandata[str(release)] = {"url": data['url'], "sha256": str(data["digests"]["sha256"])}
 
         result_path = Path(location)
         result_path.mkdir(exist_ok = True)
