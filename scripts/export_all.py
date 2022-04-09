02ccda85d9df2ac00e5fd343bd115c4bb5c8a674
j.spijker@ultimaker.com <jelle spijker>
2022-04-09 21:00:13 +0200

    Set-up auto export pipeline

---
+++ scripts/export_all.py
@@ -0,0 +1,15 @@
+import sys
+import yaml
+import subprocess
+
+from pathlib import Path
+
+if __name__ == "__main__":
+    recipes_paths = Path(__file__).absolute().parents[1].joinpath("recipes").glob("**/conandata.yml")
+    for recipe_path in recipes_paths:
+        recipe_name = recipe_path.parts[-2]
+        export_path = recipe_path.parent
+        with open(recipe_path, "r") as f:
+            parsed_yaml = yaml.safe_load(f)
+            for recipe_version in parsed_yaml["sources"].keys():
+                subprocess.run(f"conan export {export_path} {recipe_name}/{recipe_version}@{sys.argv[1]}/{sys.argv[2]}", shell = True)
--- scripts/export_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Exports all recipes found in the recipe folder
-
-pushd .
-cd ../recipes
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "conan export {}" \;
-popd
