c7f3c611da62c5cb121f2bbeb5dbf740c71f8305
Erwan MATHIEU <erwan.mathieu@ultimaker.com>
2024-11-22 15:47:57 +0100

    Use proper sentrylibrary version

--- recipes/clipper/all/conanfile.py
+++ recipes/clipper/all/conanfile.py
@@ -19,7 +19,7 @@ class ClipperConan(ConanFile):
     homepage = "http://www.angusj.com/delphi/clipper.php"
     settings = "os", "arch", "compiler", "build_type"
     package_type = "library"
-    python_requires = "sentrylibrary/1.0@ultimaker/cura_11622" # FIXME: use main after merge
+    python_requires = "sentrylibrary/1.0.0@ultimaker/cura_11622" # FIXME: use main after merge
     python_requires_extend = "sentrylibrary.SentryLibrary"
 
     options = {
